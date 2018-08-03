7561513bb6ed66ede158a8d8dba22e285fb7dd6f
John Lewis <jalewis1221@gmail.com>
2018-08-03 16:37:01 -0500

    added some new stuff

---
+++ Bootstrap-Portfolio/assets/javascript/main.js
@@ -0,0 +1,9 @@
+$.backstretch("https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3b5de7ef1f866b5c56adb3ef5d841c1c&auto=format&fit=crop&w=1350&q=80");
+
+$(".fa-github").on('click', function(){
+    window.location = "https://github.com/CandyMan999";
+});
+
+$(".fa-linkedin-square").on('click', function(){
+    window.location = "https://www.linkedin.com/in/john-lewis-b8553a65/";
+});
