ad4bf70b75b41b985d8650342dd1e2a0408d22b4
John Lewis <jalewis1221@gmail.com>
2018-10-05 14:03:02 -0500

    updated portfoilio

--- Bootstrap-Portfolio/assets/javascript/main.js
+++ Bootstrap-Portfolio/assets/javascript/main.js
@@ -1,9 +1,47 @@
-$.backstretch("https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3b5de7ef1f866b5c56adb3ef5d841c1c&auto=format&fit=crop&w=1350&q=80");
+$.backstretch(
+  "https://images.unsplash.com/photo-1507721999472-8ed4421c4af2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3b5de7ef1f866b5c56adb3ef5d841c1c&auto=format&fit=crop&w=1350&q=80"
+);
 
-$(".fa-github").on('click', function(){
-    window.location = "https://github.com/CandyMan999";
+$(".fa-github").on("click", function() {
+  window.location = "https://github.com/CandyMan999";
 });
 
-$(".fa-linkedin-square").on('click', function(){
-    window.location = "https://www.linkedin.com/in/john-lewis-b8553a65/";
+$(".fa-linkedin-square").on("click", function() {
+  window.location = "https://www.linkedin.com/in/john-lewis-b8553a65/";
+});
+
+$("#burger").on("click", function() {
+  window.location = "https://github.com/CandyMan999/burger";
+});
+
+$("#weather").on("click", function() {
+  window.location = "https://github.com/CandyMan999/weather-or-not";
+});
+
+$("#friend").on("click", function() {
+  window.location = "https://github.com/CandyMan999/friendExpress";
+});
+
+$("#guru").on("click", function() {
+  window.location = "https://github.com/CandyMan999/group_project2";
+});
+
+$("#hangman").on("click", function() {
+  window.location = "https://github.com/CandyMan999/Word-Guess-Game";
+});
+
+$("#crystal").on("click", function() {
+  window.location = "https://github.com/CandyMan999/unit-4-game";
+});
+
+$("#trivia").on("click", function() {
+  window.location = "https://github.com/CandyMan999/TriviaGame";
+});
+
+$("#rock").on("click", function() {
+  window.location = "https://github.com/CandyMan999/RPS-Multiplayer";
+});
+
+$("#giphy").on("click", function() {
+  window.location = "https://github.com/CandyMan999/giphy-API";
 });
